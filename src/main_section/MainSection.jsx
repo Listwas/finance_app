8d2d3a1418b64865c02071314fc0c0b0c6980cae
listwas <listwa.patrykkr@gmail.com>
2024-11-22 07:13:51 +0100

    temp pie chart, styling, add button

--- src/main_section/MainSection.jsx
+++ src/main_section/MainSection.jsx
@@ -4,22 +4,28 @@ function MainSection() {
     return (
         <>
             <div className={s.main_cont}>
-                <label className={s.radio_label}>
-                    <input type="radio" name="options" className={s.radio_input} />
-                    <span>day </span>
-                </label>
-                <label className={s.radio_label}>
-                    <input type="radio" name="options" className={s.radio_input} />
-                    <span>week</span>
-                </label>
-                <label className={s.radio_label}>
-                    <input type="radio" name="options" className={s.radio_input} />
-                    <span>month</span>
-                </label>
-                <label className={s.radio_label}>
-                    <input type="radio" name="options" className={s.radio_input} />
-                    <span>period</span>
-                 </label>
+                <nav>
+                    <label className={s.radio_label}>
+                        <input type="radio" name="options" className={s.radio_input} />
+                        <span>day </span>
+                    </label>
+                    <label className={s.radio_label}>
+                        <input type="radio" name="options" className={s.radio_input} />
+                        <span>week</span>
+                    </label>
+                    <label className={s.radio_label}>
+                        <input type="radio" name="options" className={s.radio_input} />
+                        <span>month</span>
+                    </label>
+                    <label className={s.radio_label}>
+                        <input type="radio" name="options" className={s.radio_input} />
+                        <span>period</span>
+                    </label>
+                </nav>
+                
+            <div className={s.chart}></div>
+            <div className={s.add_button}><button>+</button></div>
+
             </div>
         </>
     )
